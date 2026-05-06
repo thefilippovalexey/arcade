6a4bd6186d07e882a3c5eb5b01543f473320b732
thefilippovalexey <filippovwork@mail.ru>
2026-05-06 13:46:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,46 @@
-const CACHE_NAME = 'arcade-shmup-v1';
+const CACHE_NAME = 'arcade-shmup-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/arcade/',
+  '/arcade/index.html',
+  '/arcade/manifest.json',
+  '/arcade/icon-192.png',
+  '/arcade/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
